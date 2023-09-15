4918583bbdee6b045309c737f7da3efa43d362ae
Rgofman <116192052+Rgofman@users.noreply.github.com>
2023-09-14 21:47:26 -0400

    Update server.js
    added Cyclic Link

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Ruslan Gofman Student ID: 108891219 Date: 14/09/2023
+*  Cyclic Link: https://bored-suit-deer.cyclic.cloud 
+*
+********************************************************************************/ 
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
